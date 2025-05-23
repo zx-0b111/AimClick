41e135d3f333ed16a032c49d72764d3dc97b3a76
Zx <148573313+zx-0b111@users.noreply.github.com>
2025-05-23 17:04:15 -0300

    Add files via upload

---
+++ AimClick old/Clique.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DeftSharp.Windows.Input.Keyboard;
+using DeftSharp.Windows.Input.Mouse;
+using Microsoft.VisualBasic.Devices;
+
+namespace WinFormsApp1
+{
+    public class Clique
+    {
+        public static void Mouse()
+        {
+            var mouse = new MouseManipulator();
+
+            mouse.DoubleClick();
+            mouse.DoubleClick();
+            mouse.DoubleClick();
+            mouse.DoubleClick();
+            mouse.DoubleClick();
+        }
+
+        public static async Task Clicar()
+        {
+            while (true)
+            {
+                await Task.Delay(500);
+                Mouse();
+                await Task.Delay(800);
+                Mouse();
+
+                var keyboard = new KeyboardListener();
+
+                keyboard.SubscribeOnce(Key.X, key => Application.Exit());
+            }
+            
+        }
+    }
+}
---
+++ AimClick old/README.md
@@ -0,0 +1,29 @@
+
+# AimClick
+
+AutoClick simples e fácil de mexer, desenvolvido para aumentar a produção e melhorar na eficiência de farm e pvp em jogos.
+
+## Imagens abaixo para demonstração:
+
+## Imagem 1
+![Imagem 1](https://github.com/zx-0b111/AimClick/blob/main/fotos/teste1.JPG?raw=true)
+
+## Imagem 2
+![Imagem 2](https://github.com/zx-0b111/AimClick/blob/main/fotos/teste2.JPG?raw=true)
+
+
+
+
+
+
+## Autores
+
+- [@zx-0b111](https://www.github.com/zx-0b111)
+
+
+## Avisos
+
+Este código foi desenvolvido exclusivamente para fins educacionais, com o objetivo de estudar técnicas de automação, como autoclickers, e explorar o uso de APIs do Windows para interações com o sistema operacional e aplicativos.
+
+
+
